3f9062c654d7ab1b0c1f3ef63bb3e90d97fe0773
Sacha Kempeneer <sachakempeneer@msn.com>
2019-02-21 14:39:12 +0100

    Footer Update

--- application/views/main_footer.php
+++ application/views/main_footer.php
@@ -1,5 +1,79 @@
 <footer class="footer">
-      <div class="container">
-        <p class="text-muted">Place sticky footer content here.</p>
-      </div>
+    <div class="container">
+        <!-- Rij 1 -->
+        <div class="row">
+            <div class="col-sm">
+                Team 22:
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+                Opdrachtgever & Coach:
+            </div>
+        </div>
+        <!-- Rij 2 -->
+        <div class="row">
+            <div class="col-sm">
+                Michiels Maarten
+            </div>
+            <div class="col-sm">
+                Teamleider
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+                Christel Maes
+            </div>
+        </div>
+        <!-- Rij 3 -->
+        <div class="row">
+            <div class="col-sm">
+                Ingelaere Tijl
+            </div>
+            <div class="col-sm">
+                Documentbeheerder
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+
+            </div>
+        </div>
+        <!-- Rij 4 -->
+        <div class="row">
+            <div class="col-sm">
+                Kempeneer Sacha
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+
+            </div>
+        </div>
+        <!-- Rij 5 -->
+        <div class="row">
+            <div class="col-sm">
+                Michiels Jinte
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+
+            </div>
+        </div>
+    </div>
 </footer>
