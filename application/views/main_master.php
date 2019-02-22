e65698071fc3cf87483fe67595c3b3c40851b622
Sacha Kempeneer <sachakempeneer@msn.com>
2019-02-22 17:46:00 +0100

    Favicon Added

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -6,6 +6,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="/assets/images/logos/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/assets/images/logos/favicon.ico" type="image/x-icon">
 
         <title><?php echo $title ?></title>
 
