121cda5e7cba89c5fa11781a9a6090f7c3909854
jintemichiels <r0701588@student.thomasmore.be>
2019-02-28 13:39:10 +0100

    Added Login-page + link in Home controller

---
+++ application/views/Inloggen/login.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jinte
+ * Date: 28/02/2019
+ * Time: 13:36
+ */
+?>
+
+<div class="container">
+    <form>
+        <div class="form-group">
+            <label for="login">Login nummer</label>
+            <input type="text" class="form-control" id="login" placeholder="Studentennummer / Personeelsnummer">
+        </div>
+        <div class="form-group">
+            <label for="wachtwoord">Password</label>
+            <input type="password" class="form-control" id="wachtwoord" placeholder="Wachtwoord">
+        </div>
+        <button type="submit" class="btn btn-primary">Inloggen</button>
+    </form>
+</div>
