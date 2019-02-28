f4fcb5892e9112ab9d4d61a12e8fc7f3097b9000
Sacha Kempeneer <sachakempeneer@msn.com>
2019-02-28 13:48:56 +0100

    Navbar helper updated

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -1,12 +1,22 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Sacha
- * Date: 28/02/2019
- * Time: 13:24
+ * @property Home_model $home_model
  */
 
-class Home_model
+class Home_model extends CI_Model
 {
+    // +---------------------------------------------------------+ \\
+    // | ISP Project team22 - Home_model.php					 | \\
+    // +---------------------------------------------------------+ \\
+    // | 2 ITF - 2018-2019										 | \\
+    // +---------------------------------------------------------+ \\
+    // | Home model 											 | \\
+    // +---------------------------------------------------------+ \\
+    // | T.Ingelaere, S. Kempeneer, J. Michiels, M. Michiels	 | \\
+    // +---------------------------------------------------------+ \\
 
+    function __construct()
+    {
+        parent::__construct();
+    }
 }
