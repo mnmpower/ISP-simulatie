5d5ce3cd055c91b9e8e11affde15e7cceaa69b4d
Sacha Kempeneer <sachakempeneer@msn.com>
2019-03-06 13:48:22 +0100

    Fixed Header/footer, Fixed toonAfbeelding()

---
+++ application/views/wachtwoordWijzigen.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <form>
+                <div class="form-group">
+                    <label for="wachtwoordWijzigen">Login nummer</label>
+                    <input type="text" class="form-control" id="wachtwoordWijzigen" placeholder="R-nummer">
+                </div>
+                <button type="submit" class="btn btn-primary">Wijzigen</button>
+            </form>
+        </div>
+    </div>
+</div>
