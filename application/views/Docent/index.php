35d904d33e99c857bc4c19c9f8c5f0156dec74b4
jintemichiels <r0701588@student.thomasmore.be>
2019-03-27 15:14:37 +0100

    Placed Docent-index in container70

--- application/views/Docent/index.php
+++ application/views/Docent/index.php
@@ -1,26 +1,29 @@
-<h1>
-	<?php
-		echo $title
-	?>
-</h1>
-<table class="table">
-	<tbody>
-	<tr>
-		<th class="text-uppercase">Studentennummer</th>
-		<th class="text-uppercase">Naam</th>
-		<th class="text-uppercase">Aantal studiepunten</th>
-		<th class="text-uppercase">Advies</th>
-	</tr>
+<div class="container70">
+    <h1>
+        <?php
+            echo $title
+        ?>
+    </h1>
+    <table class="table">
+        <tbody>
+        <tr>
+            <th class="text-uppercase">Studentennummer</th>
+            <th class="text-uppercase">Naam</th>
+            <th class="text-uppercase">Aantal studiepunten</th>
+            <th class="text-uppercase">Advies</th>
+        </tr>
 
-	<?php
-		foreach ($ingediendeIspStudenten as $student) {
-			echo "<tr><td>$student->nummer</td>";
-			echo "<td>$student->naam</td>";
-			echo "<td>$student->studiepunten</td>";
-			echo "<td>$student->advies</td></tr>";
-		}
-	?>
+        <?php
+        foreach ($ingediendeIspStudenten as $student) {
+            echo "<tr><td>$student->nummer</td>";
+            echo "<td>$student->naam</td>";
+            echo "<td>$student->studiepunten</td>";
+            echo "<td>$student->advies</td></tr>";
+        }
+        ?>
 
-	</tbody>
-</table>
+        </tbody>
+    </table>
 
+
+</div>
