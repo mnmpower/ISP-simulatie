c2af44eef4c7af0b57e5ba4fbac8867472c558c2
jintemichiels <r0701588@student.thomasmore.be>
2019-03-27 14:23:40 +0100

    Tested 'Afspraak maken'

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -299,7 +299,7 @@
             $data['title'] = "Afspraak maken";
 
             // Defines roles for this page (You can also use "geen" or leave roles empty!).
-            $data['roles'] = getRoles('geen','geen','Ontwikkelaar','geen');
+            $data['roles'] = getRoles('geen','geen','Ontwikkelaar','Tester');
 
             // Gets buttons for navbar;
             $data['buttons'] = getNavbar('student');
